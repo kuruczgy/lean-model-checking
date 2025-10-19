af7d9622c54a668d67a77155dc0ce17f745433ed
György Kurucz <me@kuruczgy.com>
2025-10-19 07:49:16 +0200

    Add basic Lean project
    Change-Id: I7a79774ce901a8daf9e322e9f876d238d5868b07

---
+++ ModelChecking.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `ModelChecking` library.
+-- Import modules here that should be built as part of the library.
+import ModelChecking.Basic
---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      treefmt-nix,
+    }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      treefmtEval = eachSystem (
+        system:
+        (treefmt-nix.lib.evalModule nixpkgs.legacyPackages.${system} {
+          programs.nixfmt.enable = true;
+          settings.on-unmatched = "info";
+        })
+      );
+    in
+    {
+      devShells = eachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          lean4 = pkgs.lean4.overrideAttrs rec {
+            version = "4.24.0";
+            src = pkgs.fetchFromGitHub {
+              owner = "leanprover";
+              repo = "lean4";
+              tag = "v${version}";
+              hash = "sha256-m0DjKjFia5F5rCVMgn2xxPbbU/5uy7g84FUXSBPgy3w=";
+            };
+          };
+        in
+        {
+          default = pkgs.stdenv.mkDerivation {
+            name = "lean-model-checking";
+            nativeBuildInputs = [ lean4 ];
+          };
+        }
+      );
+
+      formatter = eachSystem (system: treefmtEval.${system}.config.build.wrapper);
+
+      checks = eachSystem (system: {
+        treefmt = treefmtEval.${system}.config.build.check self;
+      });
+    };
+}

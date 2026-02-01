b779c2595705c20d82062c6b268bd277e8dacf7b
György Kurucz <me@kuruczgy.com>
2026-02-01 10:23:42 +0100

    Add missing ModelChecking.SafetyLivenessDecomposition import
    Change-Id: Ib72b67527cd94d9b44546b407d20d3a1dd72853e

--- ModelChecking.lean
+++ ModelChecking.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `ModelChecking` library.
 -- Import modules here that should be built as part of the library.
 import ModelChecking.Basic
+import ModelChecking.SafetyLivenessDecomposition
